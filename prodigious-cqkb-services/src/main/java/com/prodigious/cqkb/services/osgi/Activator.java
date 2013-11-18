2be95fbb7146586cd3e34ac54f20e55e9ad450d6
Jose Castro <josedavid.castro@razorfish.com>
2013-11-18 12:00:33 -0600

    CQ maven archetype

---
+++ prodigious-cqkb-services/src/main/java/com/prodigious/cqkb/services/osgi/Activator.java
@@ -0,0 +1,19 @@
+package com.prodigious.cqkb.services.osgi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.squeakysand.osgi.framework.BasicBundleActivator;
+
+/**
+ * Bundle activator for com.prodigious.cqkb - prodigious-cqkb-services.
+ */
+public class Activator extends BasicBundleActivator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    public Activator() {
+		super(LOG);
+	}
+
+}
